b7ba8b13c595e95bdeea08580a43c65f9172c760
Haoran-Jie <2291000484@qq.com>
2024-11-05 23:02:53 +0000

    updated housing_upload_join_data for ./ file path and included year in naming

--- fynesse/access.py
+++ fynesse/access.py
@@ -80,7 +80,7 @@ def housing_upload_join_data(conn: pymysql.connections.Connection, year: int):
   cur.execute(f'SELECT pp.price, pp.date_of_transfer, po.postcode, pp.property_type, pp.new_build_flag, pp.tenure_type, pp.locality, pp.town_city, pp.district, pp.county, po.country, po.latitude, po.longitude FROM (SELECT price, date_of_transfer, postcode, property_type, new_build_flag, tenure_type, locality, town_city, district, county FROM pp_data WHERE date_of_transfer BETWEEN "' + start_date + '" AND "' + end_date + '") AS pp INNER JOIN postcode_data AS po ON pp.postcode = po.postcode')
   rows = cur.fetchall()
 
-  csv_file_path = 'output_file.csv'
+  csv_file_path = f'./output_file_{year}.csv'
 
   # Write the rows to the CSV file
   with open(csv_file_path, 'w', newline='') as csvfile:
