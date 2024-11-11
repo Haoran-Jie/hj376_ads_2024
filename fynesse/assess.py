3a7947a15d319bb87ebe09ed178d859750dea995
Haoran-Jie <2291000484@qq.com>
2024-11-11 16:51:46 +0000

    fyness assess merge and filters

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -1,6 +1,8 @@
 from .config import *
 
 from . import access
+import pandas as pd
+from .access import fetch_building_within_bbox, create_connection, fetch_houses_within_box
 
 """These are the types of import we might expect in this file
 import pandas
@@ -10,7 +12,7 @@ import matplotlib.pyplot as plt
 import sklearn.decomposition as decomposition
 import sklearn.feature_extraction"""
 
-"""Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
+"""Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes sure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
 
 
 def data():
@@ -29,3 +31,80 @@ def view(data):
 def labelled(data):
     """Provide a labelled set of data ready for supervised learning."""
     raise NotImplementedError
+
+
+def filter_full_address(buildings):
+    """Filters buildings with complete address information."""
+    address_full_condition = buildings['addr:housenumber'].notna() & buildings['addr:street'].notna() & buildings['addr:postcode'].notna()
+    return buildings[address_full_condition]
+
+def filter_building_types(buildings, non_residential, utility):
+    """Filters out non-residential and utility buildings."""
+    filtered = buildings[~buildings['building'].isin(non_residential + utility)]
+    print(len(buildings) - len(filtered), "buildings are filtered out by non-residential building types.")
+    return filtered
+
+def preprocess_street_name(df, street_col, house_col):
+    """Preprocesses the street names and house numbers for consistent matching."""
+    # Convert street name to lowercase and remove spaces
+    df.loc[:, street_col] = df.loc[:, street_col].str.lower().str.replace(" ", "")
+    # Ensure house number is integer
+    df.loc[:, house_col] = df.loc[:, house_col].astype(str)
+    return df
+
+def match_addresses(pp_df, osm_df):
+    """Matches PP and OSM data based on primary and secondary addressable object names."""
+    merged_primary = pd.merge(pp_df, osm_df, left_on=['street', 'primary_addressable_object_name'], right_on=['addr:street', 'addr:housenumber'], how='inner')
+    unmatched_pp = pp_df[~pp_df.index.isin(merged_primary.index)]
+    merged_secondary = pd.merge(unmatched_pp, osm_df, left_on=['street', 'secondary_addressable_object_name'], right_on=['addr:street', 'addr:housenumber'], how='inner')
+
+    # Remove duplicates from secondary matches based on primary match
+    duplicates = pd.merge(merged_secondary[['addr:street', 'addr:housenumber']], merged_primary[['addr:street', 'addr:housenumber']], on=['addr:street', 'addr:housenumber'], how='inner')
+    merged_secondary = merged_secondary[~merged_secondary.set_index(['addr:street', 'addr:housenumber']).index.isin(duplicates.set_index(['addr:street', 'addr:housenumber']).index)]
+
+    return pd.concat([merged_primary, merged_secondary], ignore_index=True)
+
+def filter_by_postcode(matched_df):
+    """Filters matched DataFrame by postcode agreement."""
+    original_len = len(matched_df)
+    matched_df = matched_df[matched_df['postcode'] == matched_df['addr:postcode']]
+    print("Number of matched buildings removed due to postcode mismatch:", original_len - len(matched_df))
+    return matched_df
+
+def filter_by_building_type(matched_df, valid_matches):
+    """Filters matched DataFrame by valid building type matches based on property type."""
+    original_len = len(matched_df)
+    matched_df = matched_df[
+        matched_df.apply(lambda x: x['building'] in valid_matches[x['property_type']] if valid_matches[x['property_type']] is not None else True, axis=1)
+    ]
+    print("Number of matched buildings removed due to building type mismatch:", original_len - len(matched_df))
+    return matched_df
+
+
+def filter_and_match(place_name, latitude, longitude, side_length_km, username, password, url):
+
+    non_residential_types = ['church', 'shed', 'dormitory', 'hotel', 'construction']
+    utility_types = ['entrance', 'service']
+    valid_matches = {
+        'D': ['detached', 'house', 'residential'],
+        'S': ['semidetached_house', 'house', 'residential'],
+        'T': ['terrace', 'house', 'residential'],
+        'F': ['apartments', 'house', 'residential', 'commercial'],
+        'O': None  # Allow 'O' to match with any building type
+    }
+
+    buildings, area, nodes, edges = fetch_building_within_bbox(place_name, latitude, longitude, side_length_km, draw=False)
+    conn = create_connection(username, password, url, 'ads_2024')
+    housing_pp_df = fetch_houses_within_box((latitude, longitude), 2, conn, since_date='1995-01-01')
+
+    full_address_buildings_df = filter_full_address(buildings)
+    filtered_buildings = filter_building_types(full_address_buildings_df, non_residential_types, utility_types)
+    preprocess_street_name(filtered_buildings, 'addr:street', 'addr:housenumber')
+    preprocess_street_name(housing_pp_df, 'street', 'primary_addressable_object_name')
+    preprocess_street_name(housing_pp_df, 'street', 'secondary_addressable_object_name')
+
+    final_matched_df = match_addresses(housing_pp_df, filtered_buildings)
+    final_matched_df = filter_by_postcode(final_matched_df)
+    final_matched_df = filter_by_building_type(final_matched_df, valid_matches)
+
+    return final_matched_df
