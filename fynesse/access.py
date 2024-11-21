1ac2d492f017a314d2d631f72cfb88a9c7e14356
Haoran-Jie <2291000484@qq.com>
2024-11-21 11:29:55 +0000

    updated zipfile

--- fynesse/access.py
+++ fynesse/access.py
@@ -332,21 +332,23 @@ def multi_location_poi_counts(locations_dict: dict, tags: dict, subtags: dict = 
     return combined_df
 
 def download_census_data(code, base_dir=''):
-  url = f'https://www.nomisweb.co.uk/output/census/2021/census2021-{code.lower()}.zip'
-  extract_dir = os.path.join(base_dir, os.path.splitext(os.path.basename(url))[0])
+    import zipfile
+    import io
+    url = f'https://www.nomisweb.co.uk/output/census/2021/census2021-{code.lower()}.zip'
+    extract_dir = os.path.join(base_dir, os.path.splitext(os.path.basename(url))[0])
 
-  if os.path.exists(extract_dir) and os.listdir(extract_dir):
-    print(f"Files already exist at: {extract_dir}.")
-    return
+    if os.path.exists(extract_dir) and os.listdir(extract_dir):
+        print(f"Files already exist at: {extract_dir}.")
+        return
 
-  os.makedirs(extract_dir, exist_ok=True)
-  response = requests.get(url)
-  response.raise_for_status()
+    os.makedirs(extract_dir, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
 
-  with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
-    zip_ref.extractall(extract_dir)
+    with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
+        zip_ref.extractall(extract_dir)
 
-  print(f"Files extracted to: {extract_dir}")
+    print(f"Files extracted to: {extract_dir}")
 
 def load_census_data(code, level='msoa'):
   return pd.read_csv(f'census2021-{code.lower()}/census2021-{code.lower()}-{level}.csv')
