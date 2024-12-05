d650f29e8136be4a735c844998dbe8684601c26d
Haoran-Jie <2291000484@qq.com>
2024-12-05 17:14:51 +0000

    legal

--- fynesse/access/__init__.py
+++ fynesse/access/__init__.py
@@ -3,6 +3,7 @@ from .database import create_connection, create_table, create_index, create_spat
 from .file_io import download_file, geojson_to_csv_with_wkt
 from .house_price import download_price_paid_data, fetch_houses_within_box
 from .osm import store_osm_data, get_osm_features_counts, get_osm_features_from_pbf, run_osmium_commands, fetch_building_within_bbox, count_pois_near_coordinates, count_specific_pois_near_coordinates, multi_location_poi_counts
+from .legal import show_copyright_info
 
 __all__ = [
     'download_census_data',
@@ -32,5 +33,6 @@ __all__ = [
     'fetch_building_within_bbox',
     'count_pois_near_coordinates',
     'count_specific_pois_near_coordinates',
-    'multi_location_poi_counts'
+    'multi_location_poi_counts',
+    'show_copyright_info'
 ]
---
+++ fynesse/access/legal.py
@@ -0,0 +1,28 @@
+def show_copyright_info():
+    """
+    Displays copyright and licensing information for the dataset.
+    """
+    copyright_info = """
+    © Open Database License:
+    http://opendatacommons.org/licenses/odbl/1.0/
+    Any rights in individual contents of the database are licensed under the Database Contents License:
+    http://opendatacommons.org/licenses/dbcl/1.0/
+
+    © Under the terms of the Open Government Licence (OGL) and UK Government Licensing Framework (launched 30 September 2010),
+    anyone wishing to use or re-use ONS material, whether commercially or privately, may do so freely without a specific application for a licence, 
+    subject to the conditions of the OGL and the Framework.
+
+    © Parliamentary Copyright
+    Parliament encourages you to use material made available by the House of Commons or the House of Lords in which copyright or database right subsists. 
+    Use of Parliamentary material is governed by the terms of the Open Parliament Licence:
+    https://www.parliament.uk/site-information/copyright-parliament/open-parliament-licence/
+
+    © Crown Copyright
+    All material on the Office for National Statistics (ONS) and Nomis websites is subject to Crown Copyright protection unless otherwise indicated.
+
+    © Reproducing ONS Material:
+    Under the terms of the Open Government Licence (OGL) and UK Government Licensing Framework (launched 30 September 2010),
+    anyone wishing to use or re-use ONS material, whether commercially or privately, may do so freely without a specific application for a licence, 
+    subject to the conditions of the OGL and the Framework.
+    """
+    print(copyright_info)
